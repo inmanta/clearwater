651b911808524960d076083feb891d10c20ec252
Bart Vanbrabant <bart.vanbrabant@inmanta.com>
2017-11-07 12:54:07 +0100

    Fix bootstrap issue

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -6,12 +6,18 @@
 """
 
 from inmanta.plugins import plugin, Context
-
+from inmanta.execute.proxy import UnknownException
 
 @plugin
 def instances(vnf: "clearwater::openstack::ClearwaterVNF") -> "number[]":
     """
         Return a list of instances
     """
-    instances = max(min(vnf.instances, vnf.max_instances), vnf.min_instances)
+    x = 0
+    try:
+        x = vnf.instances
+    except UnknownException as e:
+        pass
+
+    instances = max(min(x, vnf.max_instances), vnf.min_instances)
     return list(range(1, instances + 1))
